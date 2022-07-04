236bce2fec8e8f55d803994efdcf805ad836cfe7
Anant <anant312004@gmail.com>
2022-07-04 19:45:16 +0530

    Added class and functions

--- voice.py
+++ voice.py
@@ -1,4 +1,6 @@
+import pyttsx3
 import speech_recognition as sr
+
 
 class DoVoice:
 
@@ -7,11 +9,36 @@ class DoVoice:
     def __init__(self) -> None:
         ...
 
-    def take_voice() -> str:
+    def take_voice(self) -> str:
         """
         This function returns a string taking input from the user's microphone.
         """
 
+        r = sr.Recognizer()
+        with sr.Microphone() as source:
+            r.pause_threshold = 1
+            self.speak("Listening")
+            audio = r.listen(source)
+
+        try:
+            query = r.recognize_google(audio, language="en-in")
+            return query  #Returning string said by user.
+
+        except Exception as e:
+            return e
+    
+
+    @staticmethod
     def speak(string:str) -> None:
-        ...
+        """
+        This function speaks the give string.
+        """
+        # Code for tuning voice of assistant into a famale voice...
+
+        engine = pyttsx3.init('sapi5')
+        voices = engine.getProperty('voices')
+        engine.setProperty('voice', voices[0].id)
+
+        engine.say(string)
+        engine.runAndWait()
     
--- wiki.py
+++ wiki.py
@@ -1,2 +1,26 @@
 import wikipedia
 
+class Wiki:
+    """
+    This Wiki class have functions for searching wikipedia.
+    """
+
+    def __init__(self) -> None:
+        ...
+
+    @staticmethod
+    def search_wiki(search:str, sentences:int) -> str:
+        """
+        This function searches wikipedia through wikipedia library
+         It takes two arguments: 
+        search = String data which you want to search about on wikipedia
+         sentences = An Integer of how much lines you want data from wikipedia.
+
+        return value = It returns sentences of line as a string.
+        """
+
+        try:
+            return wikipedia.summary(search, sentences=sentences)
+
+        except wikipedia.exceptions.PageError:
+            print("Couldn't find about the given data.")
