b6b8c5a5f75b538e8751d3acd7b6adc977b72df5
Anant <anant312004@gmail.com>
2022-07-04 17:55:29 +0530

    Added files

---
+++ voice.py
@@ -0,0 +1,17 @@
+import speech_recognition as sr
+
+class DoVoice:
+
+    """This DoVoice class is for voice related function."""
+
+    def __init__(self) -> None:
+        ...
+
+    def take_voice() -> str:
+        """
+        This function returns a string taking input from the user's microphone.
+        """
+
+    def speak(string:str) -> None:
+        ...
+    
